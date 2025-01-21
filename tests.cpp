f9c1c631ec4c90e213002ef8a37af0f7d28756de
Ivan <ivkin2003@list.ru>
2025-01-21 14:53:21 +0300

    minor test changes

--- tests.cpp
+++ tests.cpp
@@ -4,10 +4,10 @@
 bool construcor_bad_file_test(){
 	try{
 	message_manager test2("./bad_file.txt", importance::low);
-	cout<<"CONSTRUCTOR BAD FILE TEST NOT PASSED"<<endl;
+	cout<<"Cconstrucor_bad_file_test NOT PASSED"<<endl;
 	return false;
 	}catch(const std::runtime_error &e){
-		cout<<"CONSTRUCTOR_BAD_FILE_TEST PASSED"<<endl;
+		cout<<"construcor_bad_file_test PASSED"<<endl;
 		return true;
 	}
 }
@@ -16,10 +16,10 @@ bool set_new_file_test(){
 	try{
 	message_manager test1("journal.txt",  importance::low);
 	test1.set_file_name("./bad_file.txt");
-	cout<<"SET_NEW_FILE TEST NOT PASSED"<<endl;
+	cout<<"set_new_file_test NOT PASSED"<<endl;
 	return false;
 	}catch(const std::runtime_error &e){
-		cout<<"SET_NEW_FILE_TEST PASSED"<<endl;
+		cout<<"set_new_file_test PASSED"<<endl;
 		return true;
 	}
 }
@@ -27,13 +27,14 @@ bool set_new_file_test(){
 bool bad_importance_test(){
 	try{
 		to_importance("aboba");
-		cout<<"BAD_IMPORTANCE_TEST NOT PASSED";
+		cout<<"bad_importance_test NOT PASSED";
 		return false;
 	}catch(const std::runtime_error &e){
-		cout<<"BAD_IMPORTANCE_TEST PASSED"<<endl;
+		cout<<"bad_importance_test PASSED"<<endl;
 		return true;
 	}
 }
+
 
 int main(){
 	int counter=0, summary_count=3;
